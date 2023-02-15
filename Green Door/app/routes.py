30f723de5b0b3aa2ff1a4eb147c08132a9ec52ec
SantiagoMitchell <mitchellsantiago98@gmail.com>
2023-02-15 17:11:41 +0000

    updates routes

--- Green Door/app/routes.py
+++ Green Door/app/routes.py
@@ -7,3 +7,48 @@ import sys
 @app.route('/')
 def hello():
     return render_template('homepage.html')
+
+@app.route('/about')
+def hello():
+    return render_template('about.html')
+
+@app.route('/clothingSearchHome')
+def hello():
+    return render_template('clothingSearchHome.html')
+
+@app.route('/clothingSearchResult')
+def hello():
+    return render_template('clothingSearchResult.html')
+
+@app.route('/foodSearchHome')
+def hello():
+    return render_template('foodSearchHome.html')
+
+@app.route('/foodSearchResult')
+def hello():
+    return render_template('foodSearchResult.html')
+
+@app.route('/hotelSearchHome')
+def hello():
+    return render_template('hotelSearchHome.html')
+
+@app.route('/hotelSearchResult')
+def hello():
+    return render_template('hotelSearchResult.html')
+
+@app.route('/login')
+def hello():
+    return render_template('login.html')
+
+@app.route('/profile')
+def hello():
+    return render_template('profile.html')
+
+@app.route('/siteSearchHome')
+def hello():
+    return render_template('siteSearchHome.html')
+
+@app.route('/siteSearchResults')
+def hello():
+    return render_template('siteSearchResults.html')
+
